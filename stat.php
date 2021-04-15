6d7b2ef86751e087ed8d6a428d1f39200a262b91
aujons <82440665+aujons@users.noreply.github.com>
2021-04-15 13:59:43 +0200

    test

--- stat.php
+++ stat.php
@@ -2,5 +2,6 @@
 <?php include("includes/header.php"); ?>
 <section id="stat">
             <!--här kommer kartan att visas-->
+            <h1>ajja</h1>
 </section>
 <?php include("includes/footer.php"); ?>
