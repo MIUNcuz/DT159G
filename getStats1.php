63caa5e34015738b94dd2056cb8832c17a867087
Alka1901 <alka1901@student.miun.se>
2021-04-20 14:32:28 +0200

    Testat att hämta hyran och få till array

--- getStats1.php
+++ getStats1.php
@@ -1,6 +1,7 @@
 <?php
+header('Content-Type: application/json');
 	// Länk till nedladdade REST-biblioteket. Detta ligger i mappen biblo i överordnad katalog.
-	include('httpful.phar');
+	include('biblo/httpful.phar');
 
 $url1 = "http://api.scb.se/OV0104/v1/doris/sv/ssd/START/BO/BO0406/BO0406E/BO0406Tab01";
 $url3 = "http://api.scb.se/OV0104/v1/doris/sv/ssd/BO/BO0406/BO0406E/BO0406Tab01";
@@ -18,53 +19,57 @@ $url2 = "https://kronofogden.entryscape.net/rowstore/dataset/eb9eee0d-0a3a-47e4-
 	$response_hyra2 = \Httpful\Request::get( $url3 )
 	->send();
 
-	$kronofogden = array();
+	var_dump($response_hyra);
+	
+	// $resp_arr = json_decode($response_hyra);
+	// echo($resp_arr);
+
+	//$kronofogden = array();
 	
 	
-	while( $url2 != 'INGEN_URL' )
-	{
-		$respons = restAnrop( $url2 );
-		//$inData =  json_decode( $respons );
-		$inData = $respons;
-		var_dump($inData);
-		fyllArrayer($inData, $kronofogden);
+	// while( $url2 != 'INGEN_URL' )
+	// {
+	// 	$respons = restAnrop( $url2 );
+	// 	$inData =  json_decode( $respons );
+	// 	//var_dump($inData);
+	// 	fyllArrayer($inData, $kronofogden);
 		
-		// Om det finns en länk till nästa uppsättning indata, sätt url till den länken.
-		// annars sätt url till "INGEN URL"
-		if( property_exists($inData, 'next') )
-			$url2 = $inData->next;
-		else
-			$url2 = 'INGEN_URL';
-	}
+	// 	// Om det finns en länk till nästa uppsättning indata, sätt url till den länken.
+	// 	// annars sätt url till "INGEN URL"
+	// 	if( property_exists($inData, 'next') )
+	// 		$url2 = $inData->next;
+	// 	else
+	// 		$url2 = 'INGEN_URL';
+	// }
 
 
 
 	
-	function restAnrop( $url2 )
-	{
-		$resp = \Httpful\Request::get( $url2 )
-		->send();
+	// function restAnrop( $url2 )
+	// {
+	// 	$resp = \Httpful\Request::get( $url2 )
+	// 	->send();
 		
-		return $resp;
-	}
+	// 	return $resp;
+	// }
 	
-	function fyllArrayer( &$inData, &$kronofogden)
-	{
-		// Namnen på medlemsvariablerna i kommunerna
-		//$reg="län";
-		$an_skuld="antal skuldsatta";
-		//$bel_skuld="skuldbelopp";
-		//$year="år";
+	// function fyllArrayer( &$inData, &$kronofogden)
+	// {
+	// 	// Namnen på medlemsvariablerna i kommunerna
+	// 	//$reg="län";
+	// 	$an_skuld="antal skuldsatta";
+	// 	//$bel_skuld="skuldbelopp";
+	// 	//$year="år";
 		
-    	foreach ( $inData->results as $kommun )
-		{
+    // 	foreach ( $inData->results as $län )
+	// 	{
 
-			//$kArr[] = $kommun->$reg;
-			$kronofogden[] = $kommun->$an_skuld;
-			//$kArr[] = $kommun->$bel_skuld;
-			//$kArr[] = $kommun->$year;
+	// 		//$kArr[] = $kommun->$reg;
+	// 		$kronofogden[] = $län->$an_skuld;
+	// 		//$kArr[] = $kommun->$bel_skuld;
+	// 		//$kArr[] = $kommun->$year;
 
-		}
-	}
+	// 	}
+	// }
 
 ?>
