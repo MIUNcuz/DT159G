c99e60c8f47a80b530c1dbd6b115f02f971951a4
Alka1901 <alka1901@student.miun.se>
2021-05-21 11:36:34 +0200

    Update stat.php

--- stat.php
+++ stat.php
@@ -4,33 +4,28 @@
 // filnamn: stat.php
 // Skrivet av: Erik Sillerström, August Jonsson, Alfred Karlsson
 // Kurs : Tillämpad datateknik DT159G
-// Universitet: Mitt Universitet
-// Datum: 2021-05- 
+// Universitet: Mitt Universitet 
 // Beskrivning:
 // 
-// Webbsida som visar statiskt för användaren i form av stapeldiagram och
-// linjediagram. 
+// 
+//
 //
 //////////////////////////////////////////////////////////////////////////
 
 $page_title = "Statistik";
 include("includes/header.php");
 include("includes/statmenu.php");?>
-
 <script src="Javascript/main.js"></script>
 
+
 <section id="centrum"><!--Vald region visas här --></section>
-<section id="leftStat">
-    <div id="graf"> <!-- Plotly ritas här --></div>
 
-        <div id="leftStat" class="flex"> <!-- Här visas statistik för hyran-->
-        </div>
+<section id="leftStat"> <!-- Här visas statistik för hyran-->
+</section>
 
-        <div id="rightStat" class="flex">  <!-- Här visas statistik för skulden-->
-        </div>
+<section id="rightStat">  <!-- Här visas statistik för skulden-->
+</section>
 
-        <div id="bottomStat" class="flex">    
-        </div>
-    </div>
+<section id="bottomStat">    
 </section>
 <?php include("includes/footer.php"); ?>
